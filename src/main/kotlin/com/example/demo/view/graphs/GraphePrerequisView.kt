35f8563062c1daeb3af214e6d11eba9052dc601a
gwenael <gwenael0719@gmail.com>
2021-05-18 15:08:08 +0200

    graph bigger nodes

--- src/main/kotlin/com/example/demo/view/graphs/GraphePrerequisView.kt
+++ src/main/kotlin/com/example/demo/view/graphs/GraphePrerequisView.kt
@@ -7,10 +7,10 @@ import com.example.demo.model.UESuivi
 import javafx.collections.ObservableList
 import javafx.collections.transformation.FilteredList
 import org.graphstream.graph.Graph
-import org.graphstream.graph.Node
 import org.graphstream.graph.implementations.SingleGraph
 import org.graphstream.ui.fx_viewer.FxDefaultView
 import org.graphstream.ui.fx_viewer.FxViewer
+import org.graphstream.ui.layout.springbox.BarnesHutLayout
 import org.graphstream.ui.layout.springbox.implementations.SpringBox
 import org.graphstream.ui.view.Viewer
 import tornadofx.Fragment
@@ -23,10 +23,13 @@ import java.nio.file.Paths
 
 
 class GraphePrerequisView : Fragment("Graphe des prérequis") {
-    private lateinit var suiviesParEtu: FilteredList<UESuivi>
+    var graph: Graph = SingleGraph("graph")
+    private var algo: BarnesHutLayout = SpringBox()
+    private var viewer: FxViewer = FxViewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD)
+    private var ueFormation: List<UE> = listOf()
+    private lateinit var selectedUes: FilteredList<UESuivi>
     val ctrl: MainController by inject()
     val etuCtrl: EtudiantController by inject()
-    var graph: Graph = SingleGraph("graph")
 
     override val root = vbox {
         hbox {
@@ -36,40 +39,40 @@ class GraphePrerequisView : Fragment("Graphe des prérequis") {
             label("Rouge : impossible ")
             label("Orange : sans prérequis ")
 
-
         }
         System.setProperty("org.graphstream.ui", "javafx")
     }
 
     init {
+        createGraph()
+    }
+
+    private fun createGraph() {
         print("Graphe")
         System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer")
         graph = SingleGraph("graph")
         loadCSS()
         loadGraph()
-        val viewer: Viewer = FxViewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD)
-        val algo = SpringBox()
+        viewer = FxViewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD)
         algo.stabilizationLimit = 1E-30
         algo.quality = 4.0
         algo.force = 1.7
         viewer.enableAutoLayout(algo)
-
         val view = viewer.addDefaultView(true) as FxDefaultView
         this.add(view)
     }
 
     private fun loadGraph() {
-        suiviesParEtu = ctrl.ueSuivis.filtered { ue -> ue.etu.equals(etuCtrl.selectedEtu) }
-
-        for (ue in ctrl.ues.filter { ue -> ue.formation == etuCtrl.selectedEtu.formation }) {
+        selectedUes = ctrl.ueSuivis.filtered { ue -> ue.etu.equals(etuCtrl.selectedEtu) }
+        ueFormation = ctrl.ues.filter { ue -> ue.formation == etuCtrl.selectedEtu.formation }
+        for (ue in ueFormation) {
             val node = graph.addNode(ue.code)
             node.setAttribute("ui.label", ue.code)
             node.setAttribute("layout.weight", 100)
             node.setAttribute("ui.class", "impossible")
-            node.setAttribute("ui.class", "impossible")
             drawPrerequis(ue, ue.listeUePrereq)
         }
-        for (ue in suiviesParEtu.filter { ue -> ue.ue.formation == etuCtrl.selectedEtu.formation }) {
+        for (ue in selectedUes.filter { ue -> ue.ue.formation == etuCtrl.selectedEtu.formation }) {
             val node = graph.getNode(ue.ue.code)
             node.setAttribute("ui.class", "suivie")
             if (ue.valide) {
@@ -77,56 +80,56 @@ class GraphePrerequisView : Fragment("Graphe des prérequis") {
             }
         }
         setPossible()
+        setDepart()
     }
 
-    private fun setPossible() {
-        for (node: Node in graph.nodes()) {
+    private fun setDepart() {
+        for (node in graph.nodes()) {
             if (node.enteringEdges().count() == 0L) {
                 node.setAttribute("ui.class", "depart")
             }
-            if (node.hasAttribute("ui.class")) {
-                //println(node.getAttribute("ui.class"))
-
-                var o = true
-                for (enteringNode in node.enteringEdges()) {
-                    val valid =
-                        enteringNode.hasAttribute("ui.class") && ((enteringNode.getAttribute("ui.class") as String) == "valide" || (enteringNode.getAttribute(
-                            "ui.class"
-                        ) as String) == "suivie")
-                    if (!valid) {
-                        o = false;
-                    }
-                }
-
-                println(o)
-            }
-            if (node.enteringEdges().count() != 0L &&
-                node.enteringEdges().allMatch { t ->
-                    t.hasAttribute("ui.class") &&
-                            (t.getAttribute("ui.class") as String == "valide"
-                                    || t.getAttribute("ui.class") as String == "suivie")
-                }
-            ) {
-                node.setAttribute("ui.class", "possible")
-            }
         }
-//        for (ue in ctrl.ues.filter { ue -> ue.formation == etuCtrl.selectedEtu.formation }) {
-//            println(ue.listeUePrereq.size)
-//            println(ue.code)
-//            val ueSuivi = suiviesParEtu.find { ueSuivie -> ueSuivie.ue.code == ue.code }
-//            var uePrerequisNonSuiviOuNonValide = false
-//            ue.listeUePrereq.forEach { uePrerequie ->
-//                if (suiviesParEtu.find { ueSuivi ->
-//                        ((!ueSuivi.valide || !ueSuivi.enCour) || uePrerequie.equals(ueSuivi))
-//                    } != null) {
-//                    uePrerequisNonSuiviOuNonValide = true
-//                }
-//            }
+    }
+
+    private fun setPossible() {
 //
-//            if (!uePrerequisNonSuiviOuNonValide) {
-//                graph.getNode(ue.code)?.setAttribute("ui.class", "possible")
+//            if (node.hasAttribute("ui.class")) {
+//                //println(node.getAttribute("ui.class"))
+//
+//                var o = true
+//                for (enteringNode in node.enteringEdges()) {
+//                    val valid =
+//                        enteringNode.hasAttribute("ui.class") && ((enteringNode.getAttribute("ui.class") as String) == "valide" || (enteringNode.getAttribute(
+//                            "ui.class"
+//                        ) as String) == "suivie")
+//                    if (!valid) {
+//                        o = false;
+//                    }
+//                }
+//                if (o){
+//                    node.setAttribute("ui.class", "impossible")
+//                }
+//
+//                println(o)
 //            }
 //        }
+        for (ue in ctrl.ues.filter { ue -> ue.formation == etuCtrl.selectedEtu.formation }) {
+            println(ue.listeUePrereq.size)
+            println(ue.code)
+            val ueSuiviOuValide = selectedUes.find { ueSuivie -> ueSuivie.ue.code == ue.code }
+            var uePrerequisNonSuiviOuNonValide = false
+            ue.listeUePrereq.forEach { uePrerequie ->
+                if (selectedUes.find { ueSuivi ->
+                        ((!ueSuivi.valide || !ueSuivi.enCour) || uePrerequie.equals(ueSuivi))
+                    } != null) {
+                    uePrerequisNonSuiviOuNonValide = true
+                }
+            }
+
+            if (!uePrerequisNonSuiviOuNonValide && ueSuiviOuValide == null) {
+                graph.getNode(ue.code)?.setAttribute("ui.class", "possible")
+            }
+        }
     }
 
 
